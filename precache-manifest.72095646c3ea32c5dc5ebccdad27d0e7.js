09e5026f29b0b69d94c818ba91a0451988cb725e
Ohleh <oleg.kolischuk@gmail.com>
2023-04-02 08:25:45 +0300

    Updates

---
+++ precache-manifest.72095646c3ea32c5dc5ebccdad27d0e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5f473411ee140bd393a",
+    "url": "/react-admin/static/css/main.86e75280.chunk.css"
+  },
+  {
+    "revision": "e5f473411ee140bd393a",
+    "url": "/react-admin/static/js/main.e5f47341.chunk.js"
+  },
+  {
+    "revision": "d6e15427978ddd1fcfb3",
+    "url": "/react-admin/static/js/1.d6e15427.chunk.js"
+  },
+  {
+    "revision": "417c3b6669272a363785",
+    "url": "/react-admin/static/js/runtime~main.417c3b66.js"
+  },
+  {
+    "revision": "b9f286aa7d411b3f15e7a65ba2c721da",
+    "url": "/react-admin/index.html"
+  }
+];

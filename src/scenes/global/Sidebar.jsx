29d41645bfb39c490b615ce3e3502b4430f292b0
Ohleh <oleg.kolischuk@gmail.com>
2023-04-02 22:44:39 +0300

    add collapseSidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,5 +1,82 @@
-const Sidebar = () => {
-  return <div>Sidebar</div>;
+import React from "react";
+import { useState } from "react";
+import {
+  ProSidebarProvider,
+  Sidebar,
+  Menu,
+  MenuItem,
+  useProSidebar,
+} from "react-pro-sidebar";
+// import "react-pro-sidebar/dist/css/styles.css";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+// import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+// import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+// import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+// import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+// import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+// import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+// import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+// import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+// import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+// import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+// import PieChartOutlinedIcon from "@mui/icons-material/PieChartOutlined";
+// import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+// import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+
+const Sidebarr = () => {
+  const thene = useTheme();
+  const colors = tokens(thene.palette.mode);
+  const { collapseSidebar } = useProSidebar();
+  // const [isCollapsed, setIsCollapsed] = useState(false);
+  // const [selected, useSelected] = useState("Dashboard");
+  return (
+    <Box
+      sx={{
+        "& .pro-sidebar-inner": {
+          background: `${colors.primary[400]} !important`,
+        },
+        "& .pro-icon-wrapper": {
+          backgroundColor: "transparent !important",
+        },
+        "& .pro-inner-item": {
+          padding: "5px 35px 5px 20px !important",
+        },
+        "& .pro-inner-item:hover": {
+          color: "#868dfb !important",
+        },
+        "& .pro-menu-item.active": {
+          color: "#6870fa !important",
+        },
+      }}
+    >
+      <Sidebar>
+        <Menu iconShape="square">
+          {/* LOGO & MENU ICONS */}
+          <MenuItem
+            onClick={() => collapseSidebar()}
+            icon={collapseSidebar ? <MenuOutlinedIcon /> : undefined}
+            style={{
+              margin: "10px 0 20px 0",
+              color: colors.grey[100],
+            }}
+          >
+            {!collapseSidebar && (
+              <Box>
+                <Typography variant="h3" color={colors.grey[100]}>
+                  Admin
+                </Typography>
+                <IconButton>
+                  <MenuOutlinedIcon />
+                </IconButton>
+              </Box>
+            )}
+          </MenuItem>
+        </Menu>
+      </Sidebar>
+    </Box>
+  );
 };
 
-export default Sidebar;
+export default Sidebarr;
